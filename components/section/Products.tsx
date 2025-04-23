22a547479050a5a4c353ce85d7551753e6b784c0
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2025-04-23 19:56:36 +0530

    navbar done

---
+++ components/section/Products.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Catalog from './Catalog'
+import Preview from './Preview'
+
+const Products = () => {
+  return (
+    <div>
+        <Catalog />
+        <Preview />
+    </div>
+  )
+}
+
+export default Products
